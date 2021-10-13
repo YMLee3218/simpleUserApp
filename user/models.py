b500cdee5bb41e0bc83bdbe395158f9884401ed1
BuildTools <unconfigured@null.spigotmc.org>
2021-10-13 18:34:58 +0900

    Add user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=32)
+    name = models.CharField(max_length=32)
+    phone_number = models.CharField(max_length=32)
+
+    class Meta:
+        db_table = "Users"
